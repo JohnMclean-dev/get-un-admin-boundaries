1cfcddea7da7b4f340e0ef1e1c8d4da2ad3cd823
JohnMcLean-dev <john1.mclean@outlook.com>
2026-05-12 22:26:48 -0400

    update simplified view

--- qa-queries/qa-queries.sql
+++ qa-queries/qa-queries.sql
@@ -12,14 +12,36 @@ ORDER BY pct_filled DESC, column_name ASC;
 
 -- See simplified subquery of dataset
 SELECT
+	uid,
+	
 	gid_0,
 	name_0,
-	'Country' as type_0,
+	
 	gid_1,
 	name_1,
 	type_1,
+	
 	gid_2,
 	name_2,
 	type_2,
+
+	gid_3,
+	name_3,
+	type_3,
+
+	gid_4,
+	name_4,
+	type_4,
+	
+	sovereign,
+	country,
+	continent,
+	
 	geom
-FROM public.gadm_410;
+
+FROM public.gadm_410
+
+-- WHERE
+-- 	type_4 IS NOT NULL AND type_4 <> ''
+
+LIMIT 100;
